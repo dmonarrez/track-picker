b311d64c89c621230684b1bad1c185e268590a61
David Monarrez <davidmonarrez3@gmail.com>
2019-01-18 12:31:39 -0800

    make result hidden and show on submission

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,7 @@ $(document).ready(function (){
     let totalScore = calculateScore(q1, q2, q3, q4, q5);
     let language = yourLanguage(totalScore);
 
+    $(".result").show();
     $("#language").text(language);
   });
 });
